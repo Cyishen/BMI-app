3ca5e79c8a849577921beafec43d688ff652660b
stoller <stoller@stollerde-macbook-pro.local>
2020-05-30 22:15:01 +0800

    finish

--- BMIapp/ViewController.swift
+++ BMIapp/ViewController.swift
@@ -10,11 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var heightTextField: UITextField!
+    
+    @IBOutlet weak var weightTextField: UITextField!
+    
+    @IBOutlet weak var BMILabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func calculate(_ sender: Any) {
+        let heightText = heightTextField.text!
+        let weightText = weightTextField.text!
+        let height = Double(heightText)
+        let weight = Double(weightText)
+        
+        if height != nil, weight != nil{
+            let heightMeter = height! / 100
+            let bmi = weight! / ( heightMeter * heightMeter )
+            BMILabel.text = String(format:"%.2f",bmi)
+        }
+        view.endEditing(true)
+    }
 }
 
