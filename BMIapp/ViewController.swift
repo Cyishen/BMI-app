df62a4e5bcccdc370d0b5902a1958304948dcadb
stoller <stoller@stollerde-macbook-pro.local>
2020-05-30 16:55:02 +0800

    Initial Commit

---
+++ BMIapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BMIapp
+//
+//  Created by stoller on 2020/5/30.
+//  Copyright © 2020年 cgh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
